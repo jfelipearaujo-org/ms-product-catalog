acd6110590c0fc93ae624de06b38287e95a60a24
Jose Felipe Araujo <j.felipe.araujo@outlook.com>
2024-05-19 23:12:16 -0300

    fix: :white_check_mark: fix bdd tests

--- tests/category_test.go
+++ tests/category_test.go
@@ -8,7 +8,9 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/cucumber/godog"
 	"github.com/cucumber/godog/colors"
@@ -49,30 +51,18 @@ func TestFeatures(t *testing.T) {
 }
 
 // Steps
+const featureKey CtxKeyType = "feature"
+
 type feature struct {
 	URL    string
 	Body   string
 	Status int
 }
 
-func enrichContext(ctx context.Context, feat feature) (context.Context, error) {
-	return context.WithValue(ctx, feature{}, feat), nil
-}
-
-func fromContext(ctx context.Context) (feature, error) {
-	val := ctx.Value(feature{})
-	if val == nil {
-		return feature{}, fmt.Errorf("value not found in context")
-	}
-
-	return val.(feature), nil
-}
+var state = NewState[feature](featureKey)
 
 func aCategory(ctx context.Context, categoryType string) (context.Context, error) {
-	feat, err := fromContext(ctx)
-	if err != nil {
-		return ctx, err
-	}
+	feat := state.retrieve(ctx)
 
 	if categoryType == "valid" {
 		feat.Body = `{"title": "Test Category", "description": "Test Description"}`
@@ -80,14 +70,11 @@ func aCategory(ctx context.Context, categoryType string) (context.Context, error
 		feat.Body = `{"title": "Test Category"}`
 	}
 
-	return enrichContext(ctx, feat)
+	return state.enrich(ctx, feat), nil
 }
 
 func iCreateTheCategory(ctx context.Context) (context.Context, error) {
-	feat, err := fromContext(ctx)
-	if err != nil {
-		return ctx, err
-	}
+	feat := state.retrieve(ctx)
 
 	route := fmt.Sprintf("%s/categories", feat.URL)
 	req, err := http.NewRequest(http.MethodPost, route, bytes.NewBuffer([]byte(feat.Body)))
@@ -103,14 +90,11 @@ func iCreateTheCategory(ctx context.Context) (context.Context, error) {
 
 	feat.Status = resp.StatusCode
 
-	return enrichContext(ctx, feat)
+	return state.enrich(ctx, feat), nil
 }
 
 func theCategoryIsCreated(ctx context.Context) (context.Context, error) {
-	feat, err := fromContext(ctx)
-	if err != nil {
-		return ctx, err
-	}
+	feat := state.retrieve(ctx)
 
 	if feat.Status != http.StatusCreated {
 		return ctx, fmt.Errorf("Expected status code 201, got %d", feat.Status)
@@ -120,10 +104,7 @@ func theCategoryIsCreated(ctx context.Context) (context.Context, error) {
 }
 
 func theCategoryIsNotCreated(ctx context.Context) (context.Context, error) {
-	feat, err := fromContext(ctx)
-	if err != nil {
-		return ctx, err
-	}
+	feat := state.retrieve(ctx)
 
 	if feat.Status != http.StatusUnprocessableEntity {
 		return ctx, fmt.Errorf("Expected status code 422, got %d", feat.Status)
@@ -142,118 +123,25 @@ var (
 )
 
 func InitializeScenario(ctx *godog.ScenarioContext) {
-
 	ctx.Before(func(ctx context.Context, sc *godog.Scenario) (context.Context, error) {
-		// Create a network
 		network, err := network.New(ctx, network.WithCheckDuplicate(), network.WithDriver("bridge"))
 		if err != nil {
 			return ctx, err
 		}
 
-		// Start a MongoDB container
-		mongoContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-			ContainerRequest: testcontainers.ContainerRequest{
-				Image: "mongo:7",
-				ExposedPorts: []string{
-					"27017",
-				},
-				Env: map[string]string{
-					"MONGO_INITDB_ROOT_USERNAME": "product",
-					"MONGO_INITDB_ROOT_PASSWORD": "product",
-				},
-				Networks: []string{
-					network.Name,
-				},
-				NetworkAliases: map[string][]string{
-					network.Name: {"mongo"},
-				},
-				WaitingFor: wait.ForLog("Waiting for connections"),
-			},
-			Started: true,
-		})
+		localstackContainer, ctx, err := createLocalstackContainer(ctx, network)
 		if err != nil {
 			return ctx, err
 		}
 
-		mongoIp, err := mongoContainer.Host(ctx)
+		mongoContainer, ctx, err := createMongoContainer(ctx, network)
 		if err != nil {
 			return ctx, err
 		}
 
-		mongoPort, err := mongoContainer.MappedPort(ctx, "27017")
+		apiContainer, ctx, err := createApiContainer(ctx, network)
 		if err != nil {
 			return ctx, err
-		}
-
-		connStr := fmt.Sprintf("mongodb://product:product@%s:%s/", mongoIp, mongoPort.Port())
-
-		mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(connStr))
-		if err != nil {
-			return ctx, err
-		}
-
-		err = mongoClient.Ping(ctx, nil)
-		if err != nil {
-			return ctx, err
-		}
-
-		// Start a API container
-		apiContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-			ContainerRequest: testcontainers.ContainerRequest{
-				FromDockerfile: testcontainers.FromDockerfile{
-					Context:    "../",
-					Dockerfile: "Dockerfile",
-					KeepImage:  true,
-				},
-				ExposedPorts: []string{
-					"8080",
-				},
-				Env: map[string]string{
-					"API_PORT":     "8080",
-					"API_ENV_NAME": "development",
-					"API_VERSION":  "v1",
-					"DB_NAME":      "product_db",
-					"DB_URL":       "mongodb://product:product@mongo:27017",
-				},
-				Networks: []string{
-					network.Name,
-				},
-				WaitingFor: wait.ForLog("Server started address=:8080"),
-			},
-			Started: true,
-		})
-
-		if err != nil {
-			return ctx, err
-		}
-
-		ports, err := apiContainer.Ports(ctx)
-		if err != nil {
-			return ctx, err
-		}
-
-		if len(ports["8080/tcp"]) == 0 {
-			return ctx, fmt.Errorf("Port 8080/tcp not found")
-		}
-
-		port := ports["8080/tcp"][0].HostPort
-
-		// Run health check
-		res, err := http.Get(fmt.Sprintf("http://localhost:%s/health", port))
-		if err != nil {
-			return ctx, err
-		}
-
-		if res.StatusCode != http.StatusOK {
-			body, err := io.ReadAll(res.Body)
-			if err != nil {
-				return ctx, err
-			}
-			defer res.Body.Close()
-
-			fmt.Printf("Body: %s", string(body))
-
-			return ctx, fmt.Errorf("API health check failed with status: %d", res.StatusCode)
 		}
 
 		containers[sc.Id] = testContext{
@@ -261,12 +149,11 @@ func InitializeScenario(ctx *godog.ScenarioContext) {
 			containers: []testcontainers.Container{
 				mongoContainer,
 				apiContainer,
+				localstackContainer,
 			},
 		}
 
-		return enrichContext(ctx, feature{
-			URL: fmt.Sprintf("http://localhost:%s/api/v1", port),
-		})
+		return ctx, nil
 	})
 
 	ctx.Step(`^a "([^"]*)" category$`, aCategory)
@@ -292,4 +179,172 @@ func InitializeScenario(ctx *godog.ScenarioContext) {
 
 		return ctx, err
 	})
+}
+
+func createMongoContainer(ctx context.Context, network *testcontainers.DockerNetwork) (testcontainers.Container, context.Context, error) {
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: "mongo:7",
+			ExposedPorts: []string{
+				"27017",
+			},
+			Env: map[string]string{
+				"MONGO_INITDB_ROOT_USERNAME": "product",
+				"MONGO_INITDB_ROOT_PASSWORD": "product",
+			},
+			Networks: []string{
+				network.Name,
+			},
+			NetworkAliases: map[string][]string{
+				network.Name: {"mongo"},
+			},
+			WaitingFor: wait.ForLog("Waiting for connections"),
+		},
+		Started: true,
+	})
+	if err != nil {
+		return nil, ctx, err
+	}
+
+	mongoIp, err := container.Host(ctx)
+	if err != nil {
+		return nil, ctx, err
+	}
+
+	mongoPort, err := container.MappedPort(ctx, "27017")
+	if err != nil {
+		return nil, ctx, err
+	}
+
+	connStr := fmt.Sprintf("mongodb://product:product@%s:%s/", mongoIp, mongoPort.Port())
+
+	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(connStr))
+	if err != nil {
+		return nil, ctx, err
+	}
+
+	err = mongoClient.Ping(ctx, nil)
+	if err != nil {
+		return nil, ctx, err
+	}
+
+	return container, ctx, nil
+}
+
+func createApiContainer(ctx context.Context, network *testcontainers.DockerNetwork) (testcontainers.Container, context.Context, error) {
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			FromDockerfile: testcontainers.FromDockerfile{
+				Context:    "../",
+				Dockerfile: "Dockerfile",
+				KeepImage:  true,
+			},
+			ExposedPorts: []string{
+				"8080",
+			},
+			Env: map[string]string{
+				"API_PORT":              "8080",
+				"API_ENV_NAME":          "development",
+				"API_VERSION":           "v1",
+				"DB_NAME":               "product_db",
+				"DB_URL":                "todo",
+				"DB_URL_SECRET_NAME":    "db-secret-url",
+				"AWS_ACCESS_KEY_ID":     "test",
+				"AWS_SECRET_ACCESS_KEY": "test",
+				"AWS_SESSION_TOKEN":     "test",
+				"AWS_BASE_ENDPOINT":     "http://test:4566",
+			},
+			Networks: []string{
+				network.Name,
+			},
+			WaitingFor: wait.ForLog("Server started").WithStartupTimeout(30 * time.Second),
+		},
+		Started: true,
+	})
+
+	if err != nil {
+		return nil, ctx, err
+	}
+
+	ports, err := container.Ports(ctx)
+	if err != nil {
+		return nil, ctx, err
+	}
+
+	if len(ports["8080/tcp"]) == 0 {
+		return nil, ctx, fmt.Errorf("Port 8080/tcp not found")
+	}
+
+	port := ports["8080/tcp"][0].HostPort
+
+	// Run health check
+	res, err := http.Get(fmt.Sprintf("http://localhost:%s/health", port))
+	if err != nil {
+		return nil, ctx, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			return nil, ctx, err
+		}
+		defer res.Body.Close()
+
+		fmt.Printf("Body: %s", string(body))
+
+		return nil, ctx, fmt.Errorf("API health check failed with status: %d", res.StatusCode)
+	}
+
+	return container, state.enrich(ctx, &feature{
+		URL: fmt.Sprintf("http://localhost:%s/api/v1", port),
+	}), nil
+}
+
+func createLocalstackContainer(ctx context.Context, network *testcontainers.DockerNetwork) (testcontainers.Container, context.Context, error) {
+	secretsScript, err := filepath.Abs(filepath.Join(".", "testdata", "init-sm.sh"))
+	if err != nil {
+		return nil, ctx, err
+	}
+
+	secretsScriptReader, err := os.Open(secretsScript)
+	if err != nil {
+		return nil, ctx, err
+	}
+
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: "localstack/localstack:latest",
+			ExposedPorts: []string{
+				"4566",
+			},
+			Env: map[string]string{
+				"SERVICES":       "secretsmanager",
+				"DEFAULT_REGION": "us-east-1",
+				"DOCKER_HOST":    "unix:///var/run/docker.sock",
+			},
+			Networks: []string{
+				network.Name,
+			},
+			NetworkAliases: map[string][]string{
+				network.Name: {
+					"test",
+				},
+			},
+			Files: []testcontainers.ContainerFile{
+				{
+					Reader:            secretsScriptReader,
+					ContainerFilePath: "/etc/localstack/init/ready.d/init-sns.sh",
+					FileMode:          0777,
+				},
+			},
+			WaitingFor: wait.ForListeningPort("4566/tcp").WithStartupTimeout(120 * time.Second),
+		},
+		Started: true,
+	})
+
+	if err != nil {
+		return nil, ctx, err
+	}
+
+	return container, ctx, nil
 }
